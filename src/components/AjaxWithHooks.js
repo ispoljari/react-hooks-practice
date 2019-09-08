379c3ff4e568200c5a5dab7671949536ed4c4368
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-09-08 18:37:51 +0200

    Implement controlled input in ajax with hooks component

--- src/components/AjaxWithHooks.js
+++ src/components/AjaxWithHooks.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AjaxWithHooks = () => {
-  return <h2>Ajax with Hooks - Search Engine</h2>;
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = e => {
+    const value = e.target.value;
+    setQuery(value);
+  };
+
+  return (
+    <div>
+      <h2> Ajax with Hooks - Search Engine</h2>
+      <input type="text" value={query} onChange={handleInputChange} />
+    </div>
+  );
 };
 
 export default AjaxWithHooks;
